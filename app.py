13f0b42ab0d23cef168b0d83396f0c1c0a5c817a
Олег Сергеевич Басаргин <basargin.ru@gmail.com>
2025-05-05 07:02:23 +0300

    app.py

--- app.py
+++ app.py
@@ -4,5 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Sfiral Horoscope is running!"
-# Placeholder for web app interface using Flask
+    return "Гороскоп Сфирала запущен!"
